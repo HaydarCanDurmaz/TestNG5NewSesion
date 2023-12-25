418cc71b2f837b5b2b9eb36188d491fb6fbfb34a
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-12-25 12:36:28 +0300

    Base Driver TESTNG

---
+++ src/Utlity/BaseDriver.java
@@ -0,0 +1,38 @@
+package Utlity;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class BaseDriver {
+    public static WebDriverWait wait;
+    public static WebDriver driver;
+
+    @BeforeClass
+    void BaslangicIslemleri() {
+        System.out.println(" Başlangıç İşlemleri Başladı...");
+        Logger logger = Logger.getLogger(""); // output yapılan logları al.
+        logger.setLevel(Level.SEVERE); // sadece ERROR ları göster
+
+        driver = new ChromeDriver(); // jenkins deyken : sen head olmadan yani hafızada çalış
+        //driver.manage().window().maximize(); // Ekranı max yapıyor.
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // 20 sn mühlet: sayfayı yükleme mühlet
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));  // 20 sn mühlet: elementi bulma mühleti
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+
+    @AfterClass
+    void bitisIslemleri() {
+        System.out.println("Bitiş İşlemleri yapılıyor...");
+        MyFunc.Bekle(5);
+        driver.quit();
+
+    }
+
+}
