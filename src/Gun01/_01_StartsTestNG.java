ac635cc0143de0a68c0f93dc67bfbdc6627ea3d3
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-12-25 08:26:27 +0300

    Initial commit

---
+++ src/Gun01/_01_StartsTestNG.java
@@ -0,0 +1,19 @@
+package Gun01;
+
+import org.testng.annotations.Test;
+
+public class _01_StartsTestNG {
+    @Test(priority = 1)
+    public void WebsitesiniAc(){
+
+    }
+    @Test(priority = 2)
+    public void loginislemi(){
+
+    }
+    @Test(priority = 3)
+    public void driverKapat(){
+
+    }
+
+}
